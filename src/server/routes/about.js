be74c5091d09d2b575eed65fc8b0ca0499418edf
frantic1048 <archer@frantic1048.com>
2016-01-17 20:36:33 +0800

    v0.0.7: about and its subpage

--- src/server/routes/about.js
+++ src/server/routes/about.js
@@ -2,8 +2,20 @@ import { Router } from 'express';
 
 const about = new Router();
 
-about.get('/', (req, res) => {
-  res.render('about', {activePage: 'about'});
+about.get(['/', '/index'], (req, res) => {
+  res.render('about/index', {activePage: 'about', secondaryActivePage: 'index'});
+});
+
+about.get('/teacher', (req, res) => {
+  res.render('about/teacher', {activePage: 'about', secondaryActivePage: 'teacher'});
+});
+
+about.get('/student', (req, res) => {
+  res.render('about/student', {activePage: 'about', secondaryActivePage: 'student'});
+});
+
+about.get('/contact', (req, res) => {
+  res.render('about/contact', {activePage: 'about', secondaryActivePage: 'contact'});
 });
 
 export default about;
